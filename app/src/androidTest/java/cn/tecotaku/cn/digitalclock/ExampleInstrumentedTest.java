0d404676d1c56f1365e0aa3f0246d18bc11830cd
Hoshikawa Shiro <su@geekzh.com>
2018-06-26 15:23:29 +0800

    fix test file error

--- app/src/androidTest/java/cn/tecotaku/cn/digitalclock/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/tecotaku/cn/digitalclock/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cn.tecotaku.cn.digestclock", appContext.getPackageName());
+        assertEquals("cn.tecotaku.cn.digitalclock", appContext.getPackageName());
     }
 }
